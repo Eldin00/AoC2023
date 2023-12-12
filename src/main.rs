1e62e63056f7bc26b3a96ab2bb07ea8a81341e28
Eric McKeeth <eldin00@gmail.com>
2023-12-11 23:34:10 -0700

    Add day 5 solutions

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::{
 };
 
 fn main() { 
-    let exercise_to_run: (u8, u8) = (4, 2);
+    let exercise_to_run: (u8, u8) = (5,2);
 
     match exercise_to_run {
         (1, 1) => {
@@ -30,6 +30,12 @@ fn main() {
         }
         (4, 2) => {
             d4_part2();
+        }
+        (5, 1) => {
+            d5_part1();
+        }
+        (5, 2) => {
+            d5_part2();
         }
         _ => {}
     }
@@ -386,7 +392,188 @@ fn d4_part2() {
     println!("{}", copies.values().sum::<i32>());
 }
 
+#[derive(Debug)]
+struct D5MapLine {
+    range_begin: i64,
+    range_end: i64,
+    offset: i64
+}
 
+impl D5MapLine {
+    fn parse(d: i64, s: i64, l: i64) -> D5MapLine {
+        D5MapLine { range_begin: s, range_end: s + l -1, offset: d - s }
+    }
+}
+
+#[derive(Debug)]
+struct D5Maps {
+    seed_to_soil: Vec<D5MapLine>,
+    soil_to_fert: Vec<D5MapLine>,
+    fert_to_water: Vec<D5MapLine>,
+    water_to_light: Vec<D5MapLine>,
+    light_to_temp: Vec<D5MapLine>,
+    temp_to_humid: Vec<D5MapLine>,
+    humid_to_loc: Vec<D5MapLine>,
+}
+
+impl D5Maps {
+    fn new() -> D5Maps {
+        D5Maps{                         
+                seed_to_soil: vec![],   //0
+                soil_to_fert: vec![],   //1
+                fert_to_water: vec![],  //2
+                water_to_light: vec![], //3
+                light_to_temp: vec![],  //4
+                temp_to_humid: vec![],  //5
+                humid_to_loc: vec![],   //6
+            }
+    }
+}
+
+fn d5_part1() {
+    let file = File::open("./src/d5_data.txt").unwrap();
+    let reader = BufReader::new(file);
+    let mut maps = D5Maps::new();
+    let mut seeds: Vec<i32> = vec![];
+    let mut current_map = -1;
+    let mut locations: Vec<i64> = vec![];
+
+    for line in reader.lines() {
+        let pline = line.unwrap_or(String::from(""));
+        if pline.starts_with("seeds") {
+            seeds = pline.split(" ").map(|value| value.parse::<i32>().unwrap_or(0)).collect::<Vec<i32>>()[1..].to_vec();
+        } else if pline.starts_with("seed-to-") {
+            current_map = 0;
+        } else if pline.starts_with("soil-to-") {
+            current_map = 1;
+        }else if pline.starts_with("fertilizer-to-") {
+            current_map = 2;
+        }else if pline.starts_with("water-to-") {
+            current_map = 3;
+        }else if pline.starts_with("light-to-") {
+            current_map = 4;
+        }else if pline.starts_with("temperature-to-") {
+            current_map = 5;
+        }else if pline.starts_with("humidity-to-") {
+            current_map = 6;
+        }else if pline.clone().chars().next().unwrap_or(' ').is_digit(10) {
+            let l: Vec<i64> = pline.split(" ").map(|value| value.parse::<i64>().unwrap_or(0)).collect();
+            match current_map {
+                0 => { maps.seed_to_soil.push(D5MapLine::parse(l[0], l[1], l[2]) ); },
+                1 => { maps.soil_to_fert.push(D5MapLine::parse(l[0], l[1], l[2]) ); },
+                2 => { maps.fert_to_water.push(D5MapLine::parse(l[0], l[1], l[2]) ); },
+                3 => { maps.water_to_light.push(D5MapLine::parse(l[0], l[1], l[2]) ); },
+                4 => { maps.light_to_temp.push(D5MapLine::parse(l[0], l[1], l[2]) ); },
+                5 => { maps.temp_to_humid.push(D5MapLine::parse(l[0], l[1], l[2]) ); },
+                6 => { maps.humid_to_loc.push(D5MapLine::parse(l[0], l[1], l[2]) ); },
+                _ => {}            
+            }
+        }
+    }
+    for s in seeds {
+        let mut value = s as i64;
+        for m in &maps.seed_to_soil {
+            if value >= m.range_begin && value <= m.range_end { value += m.offset; break; }
+        }
+        for m in &maps.soil_to_fert {
+            if value >= m.range_begin && value <= m.range_end { value += m.offset; break; }
+        }
+        for m in &maps.fert_to_water {
+            if value >= m.range_begin && value <= m.range_end { value += m.offset; break; }
+        }
+        for m in &maps.water_to_light {
+            if value >= m.range_begin && value <= m.range_end { value += m.offset; break; }
+        }
+        for m in &maps.light_to_temp {
+            if value >= m.range_begin && value <= m.range_end { value += m.offset; break; }
+        }
+        for m in &maps.temp_to_humid {
+            if value >= m.range_begin && value <= m.range_end { value += m.offset; break; }
+        }
+        for m in &maps.humid_to_loc {
+            if value >= m.range_begin && value <= m.range_end { value += m.offset; break; }
+        }
+        locations.push(value);
+    }
+
+    println!("{:?}", locations.iter().min().unwrap())
+
+}
+
+fn d5_part2() {
+    let file = File::open("./src/d5_data.txt").unwrap();
+    let reader = BufReader::new(file);
+    let mut maps = D5Maps::new();
+    let mut seeds: Vec<i64> = vec![];
+    let mut current_map = -1;
+
+    for line in reader.lines() {
+        let pline = line.unwrap_or(String::from(""));
+        if pline.starts_with("seeds") {
+            seeds = pline.split(" ").map(|value| value.parse::<i64>().unwrap_or(0)).filter(|v| v != &0).collect::<Vec<i64>>().to_vec();
+        } else if pline.starts_with("seed-to-") {
+            current_map = 0;
+        } else if pline.starts_with("soil-to-") {
+            current_map = 1;
+        }else if pline.starts_with("fertilizer-to-") {
+            current_map = 2;
+        }else if pline.starts_with("water-to-") {
+            current_map = 3;
+        }else if pline.starts_with("light-to-") {
+            current_map = 4;
+        }else if pline.starts_with("temperature-to-") {
+            current_map = 5;
+        }else if pline.starts_with("humidity-to-") {
+            current_map = 6;
+        }else if pline.clone().chars().next().unwrap_or(' ').is_digit(10) {
+            let l: Vec<i64> = pline.split(" ").map(|value| value.parse::<i64>().unwrap_or(0)).collect();
+            match current_map {
+                0 => { maps.seed_to_soil.push(D5MapLine::parse(l[0], l[1], l[2]) ); },
+                1 => { maps.soil_to_fert.push(D5MapLine::parse(l[0], l[1], l[2]) ); },
+                2 => { maps.fert_to_water.push(D5MapLine::parse(l[0], l[1], l[2]) ); },
+                3 => { maps.water_to_light.push(D5MapLine::parse(l[0], l[1], l[2]) ); },
+                4 => { maps.light_to_temp.push(D5MapLine::parse(l[0], l[1], l[2]) ); },
+                5 => { maps.temp_to_humid.push(D5MapLine::parse(l[0], l[1], l[2]) ); },
+                6 => { maps.humid_to_loc.push(D5MapLine::parse(l[0], l[1], l[2]) ); },
+                _ => {}            
+            }
+        }
+    }
+
+    let mut i = 0;
+    let mut min_loc: i64 = i64::max_value();
+    while i < seeds.len() {
+        for s in seeds[i]..seeds[i] + seeds[i+1] {
+            let mut value = s as i64;
+            for m in &maps.seed_to_soil {
+                if value >= m.range_begin && value <= m.range_end { value += m.offset; break; }
+            }
+            for m in &maps.soil_to_fert {
+                if value >= m.range_begin && value <= m.range_end { value += m.offset; break; }
+            }
+            for m in &maps.fert_to_water {
+                if value >= m.range_begin && value <= m.range_end { value += m.offset; break; }
+            }
+            for m in &maps.water_to_light {
+                if value >= m.range_begin && value <= m.range_end { value += m.offset; break; }
+            }
+            for m in &maps.light_to_temp {
+                if value >= m.range_begin && value <= m.range_end { value += m.offset; break; }
+            }
+            for m in &maps.temp_to_humid {
+                if value >= m.range_begin && value <= m.range_end { value += m.offset; break; }
+            }
+            for m in &maps.humid_to_loc {
+                if value >= m.range_begin && value <= m.range_end { value += m.offset; break; }
+            }
+            min_loc = if value < min_loc { value} else {min_loc};
+        }
+        i += 2;
+    }
+
+    println!("{min_loc}")
+
+}
 
 
 
